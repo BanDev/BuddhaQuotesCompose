0a160a076c511ab6fb5eb1336e89cc658e03c944
Russell Banks <Russell@bandev.uk>
2021-08-30 01:41:25 +0100

    Add basic card

--- app/src/main/java/org/bandev/buddhaquotescompose/ui/theme/Color.kt
+++ app/src/main/java/org/bandev/buddhaquotescompose/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val LighterBackground = Color(0xFF424242)
+val DarkerBackground = Color(0xFF383838)
+val Line = Color(0xFF262626)
+
+val DarkBackground = Color(0xFF222222)
--- app/src/main/java/org/bandev/buddhaquotescompose/ui/theme/Theme.kt
+++ app/src/main/java/org/bandev/buddhaquotescompose/ui/theme/Theme.kt
@@ -9,7 +9,8 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = DarkBackground
 )
 
 private val LightColorPalette = lightColors(
