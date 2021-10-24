26172b71ff0f02d55795db07746fcd1eb76d7b0f
Russell Banks <Russell@bandev.uk>
2021-10-24 02:06:09 +0100

    Add double tap to favourite

--- app/src/main/java/org/bandev/buddhaquotescompose/items/Quote.kt
+++ app/src/main/java/org/bandev/buddhaquotescompose/items/Quote.kt
@@ -20,20 +20,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package org.bandev.buddhaquotescompose.items
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import org.bandev.buddhaquotescompose.R
-import org.bandev.buddhaquotescompose.ui.theme.Shapes
-import org.bandev.buddhaquotescompose.ui.theme.Typography
-
 /**
  * An individual Quote
  */
@@ -42,38 +28,4 @@ data class Quote(
     val id: Int,
     val resource: Int,
     var liked: Boolean
-) {
-
-    @Composable
-    fun AsCard() {
-        Card(
-            modifier = Modifier.fillMaxWidth().wrapContentHeight(),
-            shape = Shapes.medium,
-            elevation = 4.dp,
-        ) {
-            Column(Modifier.padding(20.dp)) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_left_quote),
-                    contentDescription = null,
-                )
-                Text(
-                    text = stringResource(resource),
-                    style = Typography.body1
-                )
-                Box(
-                    modifier = Modifier.fillMaxWidth().wrapContentHeight(),
-                    contentAlignment = Alignment.CenterEnd
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_right_quote),
-                        contentDescription = null,
-                    )
-                }
-                Text(
-                    text = stringResource(R.string.attribution_buddha),
-                    style = Typography.caption
-                )
-            }
-        }
-    }
-}
+)
--- app/src/main/java/org/bandev/buddhaquotescompose/scenes/HomeScene.kt
+++ app/src/main/java/org/bandev/buddhaquotescompose/scenes/HomeScene.kt
@@ -2,28 +2,34 @@ package org.bandev.buddhaquotescompose.scenes
 
 import android.content.Context
 import android.content.Intent
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AddCircleOutline
-import androidx.compose.material.icons.rounded.ChevronLeft
-import androidx.compose.material.icons.rounded.ChevronRight
-import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.bandev.buddhaquotescompose.FavoriteButton
 import org.bandev.buddhaquotescompose.R
 import org.bandev.buddhaquotescompose.architecture.BuddhaQuotesViewModel
 import org.bandev.buddhaquotescompose.items.Quote
+import org.bandev.buddhaquotescompose.ui.theme.Shapes
+import org.bandev.buddhaquotescompose.ui.theme.Typography
+import org.bandev.buddhaquotescompose.ui.theme.heart
 
 @Composable
 fun HomeScene(
@@ -38,8 +44,65 @@ fun HomeScene(
     )
     var isLiked by remember { mutableStateOf(quote.liked) }
     val context = LocalContext.current
+    var isAnimating by remember { mutableStateOf(false) }
+    val animatedSize by animateDpAsState(
+        targetValue = if (isAnimating) 100.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = 500f
+        )
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = {
+                        isAnimating = true
+                        if (!isLiked) {
+                            isLiked = !isLiked
+                            viewModel.Quotes().setLike(quote.id, isLiked)
+                        }
+                    }
+                )
+            }
+    )
     Column(Modifier.padding(start = 15.dp, top = 1.dp, end = 15.dp)) {
-        quote.AsCard()
+        Crossfade(targetState = quote) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                shape = Shapes.medium,
+                elevation = 4.dp,
+            ) {
+                Column(Modifier.padding(20.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_left_quote),
+                        contentDescription = null,
+                    )
+                    Text(
+                        text = stringResource(it.resource),
+                        style = Typography.body1
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        contentAlignment = Alignment.CenterEnd
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_right_quote),
+                            contentDescription = null,
+                        )
+                    }
+                    Text(
+                        text = stringResource(R.string.attribution_buddha),
+                        style = Typography.caption
+                    )
+                }
+            }
+        }
         Column(
             Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -48,7 +111,7 @@ fun HomeScene(
             Image(
                 painter = painterResource(id = R.drawable.image_anahata),
                 contentDescription = null,
-                modifier = Modifier.size(250.dp)
+                modifier = Modifier.size(200.dp)
             )
             Card(
                 modifier = Modifier.padding(20.dp),
@@ -116,6 +179,21 @@ fun HomeScene(
                         )
                     }
                 }
+            }
+        }
+    }
+    Box(Modifier.fillMaxSize()) {
+        if (isAnimating) {
+            Icon(
+                imageVector = Icons.Rounded.Favorite,
+                tint = heart,
+                modifier = Modifier
+                    .size(animatedSize)
+                    .align(Alignment.Center),
+                contentDescription = null
+            )
+            if (animatedSize == 100.dp) {
+                isAnimating = false
             }
         }
     }
